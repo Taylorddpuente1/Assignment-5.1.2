bd712a4a7ebdd5d14e18666a6c599e807b1e976f
Taylo <Taylo@MARIEL>
2025-05-06 16:17:26 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// Write a program to create a function to calculate the sum of individual digits of a given number.
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+class Program
+{
+    static void Main()
+    { 
+ Console.WriteLine(" Please enter numbers:  ");
+  int myNum = Convert.ToInt32(Console.ReadLine());
+
+        int sum = 0;
+        while  (myNum > 0)
+        {
+            sum = sum + myNum % 10; 
+            myNum  /= 10;
+        }
+        Console.WriteLine("The sum of the digits is : " + sum);
+        }
+    }
+
